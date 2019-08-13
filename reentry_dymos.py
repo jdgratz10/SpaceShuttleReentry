0d34ab494488dee1f458def698da97bfe53c5e50
openmdao <openmdao@openmdaombp.ndc.nasa.gov>
2019-08-13 10:03:53 -0400

    fully convergenct and correct manually scaled solution

--- reentry_dymos.py
+++ reentry_dymos.py
@@ -11,14 +11,14 @@ traj = prob.model.add_subsystem("traj", Trajectory())
 phase0 = Phase(ode_class=ShuttleODE, transcription=Radau(num_segments=50, order=3))
 traj.add_phase(name="phase0", phase=phase0)
 
-phase0.set_time_options(fix_initial=True, units="s", duration_ref=200)#, duration_ref=2000, duration_bounds=(50, 3000)
+phase0.set_time_options(fix_initial=True, units="s", duration_ref=200)
 
-phase0.set_state_options("h", fix_initial=True, fix_final=True, units="ft", rate_source="hdot", targets=["h"], lower=0, ref0=75000, ref=300000, defect_ref=1000)#, ref=260000, defect_ref=260000, ref0=80000
+phase0.set_state_options("h", fix_initial=True, fix_final=True, units="ft", rate_source="hdot", targets=["h"], lower=0, ref0=75000, ref=300000, defect_ref=1000)
 phase0.set_state_options("gamma", fix_initial=True, fix_final=True, units="rad", rate_source="gammadot", targets=["gamma"], lower=-89.*np.pi/180, upper=89.*np.pi/180)
 phase0.set_state_options("phi", fix_initial=True, fix_final=False, units="rad", rate_source="phidot", lower=0, upper=89.*np.pi/180)
 phase0.set_state_options("psi", fix_initial=True, fix_final=False, units="rad", rate_source="psidot", targets=["psi"], lower=0, upper=90.*np.pi/180)
 phase0.set_state_options("theta", fix_initial=True, fix_final=False, units="rad", rate_source="thetadot", targets=["theta"], lower=-89.*np.pi/180, upper=89.*np.pi/180)
-phase0.set_state_options("v", fix_initial=True, fix_final=True, units="ft/s", rate_source="vdot", targets=["v"], lower=0, ref0=2500, ref=25000)#, ref=25600, defect_ref=25600, ref0=2500
+phase0.set_state_options("v", fix_initial=True, fix_final=True, units="ft/s", rate_source="vdot", targets=["v"], lower=0, ref0=2500, ref=25000)
 
 phase0.add_control("alpha", units="rad", opt=True, lower=-np.pi/2, upper=np.pi/2, targets=["alpha"])
 phase0.add_control("beta", units="rad", opt=True, lower=-89*np.pi/180, upper=1*np.pi/180, targets=["beta"])
@@ -132,16 +132,8 @@ plt.plot(prob.get_val("traj.phase0.timeseries.time", units="s"),
          prob.get_val("traj.phase0.timeseries.q"), "ro", label="Solution")
 plt.plot(sim_out.get_val("traj.phase0.timeseries.time", units="s"),
          sim_out.get_val("traj.phase0.timeseries.q"), "b-", label="Simulation")
-
-# plt.plot(np.arange(len(final_q)), final_q)
 plt.title("Heating rate over Time")
 plt.xlabel("Time points")
 plt.ylabel("Heating Rate (BTU/ft**2/s")
 
 plt.show()
-
-# print(prob.get_val("traj.phase0.collocation_constraint.defects:v"))
-print("final time is: ", prob.get_val("traj.phase0.timeseries.time")[-1])
-print("final h is: ", prob.get_val("traj.phase0.timeseries.states:h")[-1])
-print("final gamma is: ", prob.get_val("traj.phase0.timeseries.states:gamma", units="deg")[-1])
-print("final v is: ", prob.get_val("traj.phase0.timeseries.states:v")[-1])
