5543094d1a57b1b4c023f30650d2c94488591540
openmdao <openmdao@openmdaombp.ndc.nasa.gov>
2019-08-13 17:03:23 -0400

    implemented autoscaler and added bounds file, autoscaler results in division by zero error

---
+++ bounds_file.py
@@ -0,0 +1,40 @@
+import pickle
+import numpy as np
+
+lbs = {}
+ubs = {}
+
+ubs["traj.phases.phase0.time_extents.t_duration"] = 2000.
+ubs["traj.phases.phase0.indep_states.states:h"] = 260000.
+ubs["traj.phases.phase0.indep_states.states:gamma"] = -1.*np.pi/180.
+ubs["traj.phases.phase0.indep_states.states:phi"] = 75.*np.pi/180.
+ubs["traj.phases.phase0.indep_states.states:psi"] = 90.*np.pi/180.
+ubs["traj.phases.phase0.indep_states.states:theta"] = 25.*np.pi/180.
+ubs["traj.phases.phase0.indep_states.states:v"] = 25600.
+ubs["traj.phases.phase0.control_group.indep_controls.controls:alpha"] = 17.4*np.pi/180.
+ubs["traj.phases.phase0.control_group.indep_controls.controls:beta"] = 0.1*np.pi/180.
+
+lbs["traj.phases.phase0.time_extents.t_duration"] = .01
+lbs["traj.phases.phase0.indep_states.states:h"] = 80000.
+lbs["traj.phases.phase0.indep_states.states:gamma"] = -5.*np.pi/180.
+lbs["traj.phases.phase0.indep_states.states:phi"] = 0.
+lbs["traj.phases.phase0.indep_states.states:psi"] = 10.*np.pi/180.
+lbs["traj.phases.phase0.indep_states.states:theta"] = 0.
+lbs["traj.phases.phase0.indep_states.states:v"] = 2500.
+lbs["traj.phases.phase0.control_group.indep_controls.controls:alpha"] = 17.4*np.pi/180.
+lbs["traj.phases.phase0.control_group.indep_controls.controls:beta"] = -75.*np.pi/180.
+
+with open("lower_bounds_info.pickle", "wb") as file:
+    pickle.dump(lbs, file, protocol=pickle.HIGHEST_PROTOCOL)
+with open("upper_bounds_info.pickle", "wb") as file:
+    pickle.dump(ubs, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+# with open("lower_bounds_info.pickle", "rb") as file:
+#     file1 = pickle.load(file)
+
+# with open("upper_bounds_info.pickle", "rb") as file:
+#     file2 = pickle.load(file)
+# print("lower bounds are: ")
+# print(file1)
+# print("upper bounds are: ")
+# print(file2)
--- reentry_dymos.py
+++ reentry_dymos.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from openmdao.api import Problem, Group, ScipyOptimizeDriver, SqliteRecorder, CaseReader, IndepVarComp, pyOptSparseDriver
 from dymos import Trajectory, GaussLobatto, Phase, Radau
 from shuttle_ode import ShuttleODE
+import pickle
+from autoscaling.api import autoscale, PJRNScaler, IsoScaler
 
 prob = Problem(model=Group())
 
@@ -13,19 +15,19 @@ traj.add_phase(name="phase0", phase=phase0)
 
 phase0.set_time_options(fix_initial=True, units="s", duration_ref=200)
 
-phase0.set_state_options("h", fix_initial=True, fix_final=True, units="ft", rate_source="hdot", targets=["h"], lower=0, ref0=75000, ref=300000, defect_ref=1000)
+phase0.set_state_options("h", fix_initial=True, fix_final=True, units="ft", rate_source="hdot", targets=["h"], lower=0)
 phase0.set_state_options("gamma", fix_initial=True, fix_final=True, units="rad", rate_source="gammadot", targets=["gamma"], lower=-89.*np.pi/180, upper=89.*np.pi/180)
 phase0.set_state_options("phi", fix_initial=True, fix_final=False, units="rad", rate_source="phidot", lower=0, upper=89.*np.pi/180)
 phase0.set_state_options("psi", fix_initial=True, fix_final=False, units="rad", rate_source="psidot", targets=["psi"], lower=0, upper=90.*np.pi/180)
 phase0.set_state_options("theta", fix_initial=True, fix_final=False, units="rad", rate_source="thetadot", targets=["theta"], lower=-89.*np.pi/180, upper=89.*np.pi/180)
-phase0.set_state_options("v", fix_initial=True, fix_final=True, units="ft/s", rate_source="vdot", targets=["v"], lower=0, ref0=2500, ref=25000)
+phase0.set_state_options("v", fix_initial=True, fix_final=True, units="ft/s", rate_source="vdot", targets=["v"], lower=0)
 
 phase0.add_control("alpha", units="rad", opt=True, lower=-np.pi/2, upper=np.pi/2, targets=["alpha"])
 phase0.add_control("beta", units="rad", opt=True, lower=-89*np.pi/180, upper=1*np.pi/180, targets=["beta"])
 
-phase0.add_path_constraint("q", lower=0, upper=70, units="Btu/ft**2/s", ref=70)
+phase0.add_path_constraint("q", lower=0, upper=70, units="Btu/ft**2/s")
 
-phase0.add_objective("theta", loc="final", ref=-0.01)
+phase0.add_objective("theta", loc="final", ref=-1)
 
 prob.driver = pyOptSparseDriver()
 prob.driver.declare_coloring()
@@ -49,8 +51,31 @@ prob.set_val("traj.phase0.controls:beta", phase0.interpolate(ys=[-75*np.pi/180, 
 recorder = SqliteRecorder("reentry.sql")
 prob.driver.add_recorder(recorder)
 
+with open("total_jac_info.pickle", "rb") as file:
+    J = pickle.load(file)
+with open("lower_bounds_info.pickle", "rb") as file:
+    lower_bounds = pickle.load(file)
+with open("upper_bounds_info.pickle", "rb") as file:
+    upper_bounds = pickle.load(file)
+
+#Note:current jacobian in total_jac_info.pickle is from a run_model case and 
+#cannot be used for PJRNScaler (refs that lead to convergence can be found on
+# master branch and can be implemented to get jacobian of fully converged model)
+
+# autoscaler = None
+autoscaler = IsoScaler(J, lower_bounds, upper_bounds)
+# autoscaler = PJRNScaler(J, lower_bounds, upper_bounds)
+
+autoscale(prob, autoscaler=autoscaler)
+
 prob.run_driver()
 # prob.run_model()
+
+#The following lines only need to be run once to get the initial jacobian. They may
+#be run with a run_model() call if only using IsoScaler, but need a fully converged
+#run_driver() call if using PJRNScaler.
+# with open("total_jac_info.pickle", "wb") as file:
+#     pickle.dump(prob.compute_totals(), file, protocol=pickle.HIGHEST_PROTOCOL)
 
 sim_out = traj.simulate()
 
