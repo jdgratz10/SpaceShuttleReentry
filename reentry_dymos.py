3f354a809405ea13475029fc073991f5ca6fe9db
openmdao <openmdao@openmdaombp.ndc.nasa.gov>
2019-08-16 13:06:43 -0400

    implemented autoscaling updates, still doesn't run properly

--- reentry_dymos.py
+++ reentry_dymos.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from openmdao.api import Problem, Group, ScipyOptimizeDriver, SqliteRecorder, CaseReader, IndepVarComp, pyOptSparseDriver
 from dymos import Trajectory, GaussLobatto, Phase, Radau
 from shuttle_ode import ShuttleODE
-import pickle
 from autoscaling.api import autoscale, PJRNScaler, IsoScaler
 
 prob = Problem(model=Group())
@@ -13,21 +12,21 @@ traj = prob.model.add_subsystem("traj", Trajectory())
 phase0 = Phase(ode_class=ShuttleODE, transcription=Radau(num_segments=50, order=3))
 traj.add_phase(name="phase0", phase=phase0)
 
-phase0.set_time_options(fix_initial=True, units="s", duration_ref=200)
+phase0.set_time_options(fix_initial=True, units="s", duration_bounds=(1000, 3000), duration_ref=200)
 
-phase0.set_state_options("h", fix_initial=True, fix_final=True, units="ft", rate_source="hdot", targets=["h"], lower=0)
-phase0.set_state_options("gamma", fix_initial=True, fix_final=True, units="rad", rate_source="gammadot", targets=["gamma"], lower=-89.*np.pi/180, upper=89.*np.pi/180)
+phase0.set_state_options("h", fix_initial=True, fix_final=True, units="ft", rate_source="hdot", targets=["h"], lower=0, upper=300000)#, ref0=75000, ref=300000, defect_ref=1000
+phase0.set_state_options("gamma", fix_initial=True, fix_final=True, units="rad", rate_source="gammadot", targets=["gamma"], lower=-20.*np.pi/180, upper=20.*np.pi/180)
 phase0.set_state_options("phi", fix_initial=True, fix_final=False, units="rad", rate_source="phidot", lower=0, upper=89.*np.pi/180)
 phase0.set_state_options("psi", fix_initial=True, fix_final=False, units="rad", rate_source="psidot", targets=["psi"], lower=0, upper=90.*np.pi/180)
 phase0.set_state_options("theta", fix_initial=True, fix_final=False, units="rad", rate_source="thetadot", targets=["theta"], lower=-89.*np.pi/180, upper=89.*np.pi/180)
-phase0.set_state_options("v", fix_initial=True, fix_final=True, units="ft/s", rate_source="vdot", targets=["v"], lower=0)
+phase0.set_state_options("v", fix_initial=True, fix_final=True, units="ft/s", rate_source="vdot", targets=["v"], lower=0, upper=30000)#, ref0=2500, ref=25000
 
-phase0.add_control("alpha", units="rad", opt=True, lower=-np.pi/2, upper=np.pi/2, targets=["alpha"])
+phase0.add_control("alpha", units="rad", opt=True, lower=-np.pi/4, upper=np.pi/4, targets=["alpha"])
 phase0.add_control("beta", units="rad", opt=True, lower=-89*np.pi/180, upper=1*np.pi/180, targets=["beta"])
 
-phase0.add_path_constraint("q", lower=0, upper=70, units="Btu/ft**2/s")
+phase0.add_path_constraint("q", lower=0, upper=70, units="Btu/ft**2/s")#, ref=70
 
-phase0.add_objective("theta", loc="final", ref=-1)
+phase0.add_objective("theta", loc="final")#, ref=-0.01
 
 prob.driver = pyOptSparseDriver()
 prob.driver.declare_coloring()
@@ -51,25 +50,36 @@ prob.set_val("traj.phase0.controls:beta", phase0.interpolate(ys=[-75*np.pi/180, 
 recorder = SqliteRecorder("reentry.sql")
 prob.driver.add_recorder(recorder)
 
-with open("total_jac_info.pickle", "rb") as file:
-    J = pickle.load(file)
-with open("lower_bounds_info.pickle", "rb") as file:
-    lower_bounds = pickle.load(file)
-with open("upper_bounds_info.pickle", "rb") as file:
-    upper_bounds = pickle.load(file)
+# scaler = PJRNScaler(prob)
+# for name, ref0 in scaler.ref0s.items():
+#     print(name, ref0, scaler.refs[name])
 
-#Note:current jacobian in total_jac_info.pickle is from a run_model case and 
-#cannot be used for PJRNScaler (refs that lead to convergence can be found on
-# master branch and can be implemented to get jacobian of fully converged model)
+# for name, defect_ref in scaler.defect_refs.items():
+#     print(name, defect_ref, "\n")
 
-# autoscaler = None
-autoscaler = IsoScaler(J, lower_bounds, upper_bounds)
-# autoscaler = PJRNScaler(J, lower_bounds, upper_bounds)
+# exit()
+prob.run_model()
+exp_out = traj.simulate()
+t_exp = exp_out.get_val("traj.phase0.timeseries.time")
+h_out = exp_out.get_val("traj.phase0.timeseries.states:h")
+gamma_out = exp_out.get_val("traj.phase0.timeseries.states:gamma")
+phi_out = exp_out.get_val("traj.phase0.timeseries.states:phi")
+psi_out = exp_out.get_val("traj.phase0.timeseries.states:psi")
+theta_out = exp_out.get_val("traj.phase0.timeseries.states:theta")
+v_out = exp_out.get_val("traj.phase0.timeseries.states:v")
 
-autoscale(prob, autoscaler=autoscaler)
+prob.set_val("traj.phase0.states:h", phase0.interpolate(xs=t_exp, ys=h_out, nodes="state_input"), units="ft")
+prob.set_val("traj.phase0.states:gamma", phase0.interpolate(xs=t_exp, ys=gamma_out, nodes="state_input"), units="rad")
+prob.set_val("traj.phase0.states:phi", phase0.interpolate(xs=t_exp, ys=phi_out, nodes="state_input"), units="rad")
+prob.set_val("traj.phase0.states:psi", phase0.interpolate(xs=t_exp, ys=psi_out, nodes="state_input"), units="rad")
+prob.set_val("traj.phase0.states:theta", phase0.interpolate(xs=t_exp, ys=theta_out, nodes="state_input"), units="rad")
+prob.set_val("traj.phase0.states:v", phase0.interpolate(xs=t_exp, ys=v_out, nodes="state_input"), units="ft/s")
 
-prob.run_driver()
-# prob.run_model()
+
+autoscale(prob, autoscaler=PJRNScaler(prob))
+
+# prob.run_driver()
+prob.run_model()
 
 #The following lines only need to be run once to get the initial jacobian. They may
 #be run with a run_model() call if only using IsoScaler, but need a fully converged
@@ -81,12 +91,12 @@ sim_out = traj.simulate()
 
 prob.cleanup()
 
-case_reader = CaseReader("reentry.sql")
+# case_reader = CaseReader("reentry.sql")
 
-driver_cases = case_reader.list_cases("driver")
-last_case = case_reader.get_case(driver_cases[-1])
-final_constraints = last_case.get_constraints()
-final_q = final_constraints["traj.phase0.path_constraints.path:q"]
+# driver_cases = case_reader.list_cases("driver")
+# last_case = case_reader.get_case(driver_cases[-1])
+# final_constraints = last_case.get_constraints()
+# final_q = final_constraints["traj.phase0.path_constraints.path:q"]
 
 plt.figure(0)
 plt.plot(prob.get_val("traj.phase0.timeseries.time", units="s"), prob.get_val("traj.phase0.timeseries.controls:alpha", units="deg"), "ro", label="Solution")
